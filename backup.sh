e451e46af54e178357023c8ca5b4c020cb688576
David G. Simmons <santafen@mac.com>
2017-03-15 12:18:35 -0400

    Fixed disk reformat
    In case your disk is laid out weird ... this will always find the end
    of partition 2, and the max sector, and set partition 3 to max size.

--- backup.sh
+++ backup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# The name of the tar file
+TARFILE=/mnt/SD3/rootfs.tar
 
 # Progress spinner for feedback
 sp="/-\|"
@@ -13,6 +15,11 @@ endspin() {
 }
 
 # Expand partition 3 of the SD Card to make it as large as possible
+start_sector=`fdisk -l /dev/mmcblk1 | grep mmcblk1p3 | awk '{print $2}'`
+end_sector=`fdisk -l /dev/mmcblk1 | awk 'NR==1 {print $7}'`
+
+end_sector=$((end_sector - 1))
+echo $end_sector
 DEV="/dev/mmcblk1"
 echo "Resizing partition map for $DEV"
 for disk in ${DEV}*
@@ -27,8 +34,8 @@ d
 n
 p
 
-
-
+$start_sector
+$end_sector
 w
 EOF
 
@@ -38,6 +45,7 @@ e2fsck -f -y ${DEV}p3
 resize2fs ${DEV}p3
 
 sync; sync
+
 
 echo "Resize complete"
 echo "Mounting new Filesystem..."
@@ -71,36 +79,77 @@ else
     exit 1
 fi
 
-cd /
-echo "Backing up root file system. This will take some time."
-
-TARFILE=/mnt/SD3/rootfs.tar
-touch $TARFILE
-
 # These are all added to the tarfile with --no-recursion
 ADD_DIRS="bin dev/pts lib lost+found media mnt proc run/systemd run/systemd/shutdown run/systemd/netif run/systemd/netif/links run/systemd/netif/leases run/systemd/machines run/systemd/sessions run/systemd/ask-password run/systemd/users run/systemd/seats run/mariadb run/log run/wpa_supplicant run/lock run/lock/subsys run/lock/lockdev run/user run/faillock run/setrans run/lirc run/console run/netreport run/blkid run/sepermit sbin srv sys var/log var/lock var/run var/cache/yum var/cache/dnf var/yp var/games var/empty var/empty/sshd var/tmp var/spool var/spool/mail var/spool/mail/rpc var/spool/lpd var/spool/anacron var/spool/cron"
 
+# See if the user wants to add more
+user_add_dir=''
+read -p 'Enter additonal (non-recursive) Directory: ' newdir
+while [ ! -z $newdir ]; do
+    user_add_dir+=" $newdir"
+    read -p 'Enter additonal (non-recursive) Directory: ' newdir
+    if [ -z $newdir ]; then
+        break
+    fi
+done
+
+if [ ! -z user_add_dir ]; then
+    ADD_DIRS+=" $user_add_dir"
+fi
+
 # These are added recursively. 
-ADD_FILES="etc dev/stdin dev/stderr dev/tty dev/random dev/fd dev/zero dev/full dev/ptmx dev/shm/ dev/urandom dev/null dev/stdout home opt root tmp/.font-unix/ tmp/.Test-unix/ tmp/.XIM-unix/ tmp/.X11-unix/ tmp/.ICE-unix/ usr var/local/ var/nis/ var/log/lastlog var/log/mariadb/ var/log/wtmp var/log/btmp var/log/tallylog var/log/README var/log/journal/ var/lib/ var/mail var/preserve/ var/opt/ var/gopher/ var/kerberos/ var/db/ var/adm/ var/cache/ var/cache/ldconfig/ var/cache/ldconfig/aux-cache var/cache/man/ var/cache/fontconfig/ var/spool/anacron/cron.weekly var/spool/anacron/cron.daily var/spool/anacron/cron.monthly tmp/.kura"
+ADD_FILES="etc dev/stdin dev/stderr dev/tty dev/random dev/fd dev/zero dev/full dev/ptmx dev/shm/ dev/urandom dev/null dev/stdout home opt root tmp/.font-unix/ tmp/.Test-unix/ tmp/.XIM-unix/ tmp/.X11-unix/ tmp/.ICE-unix/ usr var/local/ var/nis/ var/log/lastlog var/log/mariadb/ var/log/wtmp var/log/btmp var/log/tallylog var/log/README var/log/journal/ var/lib/ var/mail var/preserve/ var/opt/ var/gopher/ var/kerberos/ var/db/ var/adm/ var/cache/ var/cache/ldconfig/ var/cache/ldconfig/aux-cache var/cache/man/ var/cache/fontconfig/ var/spool/anacron/cron.weekly var/spool/anacron/cron.daily var/spool/anacron/cron.monthly"
+
+# See if the user wants to add more
+user_add_file=''
+read -p 'Enter additonal (recursive) Directory or File: ' newfile
+while [ ! -z newfile ]; do
+    user_add_file+=" $newfile"
+    read -p 'Enter additonal (recursive) Directory or File: ' newfile
+    if [ -z $newfile ]; then
+        break
+    fi
+done
+
+if [ ! -z user_add_file ]; then
+    ADD_FILES+=" $user_add_file"
+fi
+
+
+cd /
+echo "Backing up root file system. This will take some time."
+
+
+touch $TARFILE
+
+
+
 
 for f in $ADD_DIRS ; do
     if [ $pv ]; then 
-        echo -ne  "`tar rf $TARFILE --no-recursion $f | pv -N \"Adding $f \"   ` \r"
+        if [ -d $f -o -f $f ]; then
+            echo -ne  "`tar rf $TARFILE --no-recursion $f | pv -N \"Adding $f \"   ` \r"
+        fi
     else
         echo "Adding $f to tarfile ..."
-        tar rf $TARFILE --no-recursion $f  
+        if [ -d $f -o -f $f ]; then
+            tar rf $TARFILE --no-recursion $f  
+        fi
     fi
 done 
 echo  "Done with directories                                     "
 
 for f in $ADD_FILES ; do
     if [ $pv ]; then
-        #spin
-        echo -ne "`tar cf - $f -P | pv -N \"Adding $f \" -s  $(du -sb $f | awk '{print $1}') >> $TARFILE`                    \r"               
+        if [ -d $f -o -f $f ]; then
+            echo -ne "`tar cf - $f -P | pv -N \"Adding $f \" -s  $(du -sb $f | awk '{print $1}') >> $TARFILE`                    \r"  
+        fi             
     else
-        printf "Adding %s \n" "$f"
-        tar rf $TARFILE --checkpoint=.1000 $f 
-        printf "\n"
+        if [ -d $f -o -f $f ]; then
+            printf "Adding %s \n" "$f"
+            tar rf $TARFILE --checkpoint=.1000 $f 
+            printf "\n"
+        fi
     fi
 done 
 echo  "Done with files                                          "
